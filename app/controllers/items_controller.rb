4adcc4f225bfad7a88242beb70d2f71d5dcb1a0e
kr95656 <katmsg8239@gmail.com>
2020-05-20 14:21:03 +0900

    initial commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,22 @@
 class ItemsController < ApplicationController
+  
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :image, :text)
+    
+  end
+
+
 end
