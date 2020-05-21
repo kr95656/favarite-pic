7f573bb3812e4bf7f99f17c3d96323d0ae34fe90
kr95656 <katmsg8239@gmail.com>
2020-05-21 21:37:06 +0900

    投稿された写真が各タグのviewに反映されるようにデザイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
-  resources :items, only: [:index, :new, :create, :destroy]
+  resources :tags,  only: [:index, :new, :create, :show] 
+  resources :items, only: [:index, :new, :create, :destroy] 
   resources :users, only: [:edit, :update]
-  resources :tags,  only: [:index, :new, :create]
 end
