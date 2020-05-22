5010427bb511ce72521d74b590e3a654ec3dd94b
kr95656 <katmsg8239@gmail.com>
2020-05-22 20:06:30 +0900

    gemfile changed

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :tags,  only: [:index, :new, :create, :show] 
   resources :items, only: [:index, :new, :create, :destroy, :show, :edit, :update] do
     resources :comments, only: [:create]
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: [:edit, :update, :show]
 end
