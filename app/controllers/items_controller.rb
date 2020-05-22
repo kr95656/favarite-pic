345371a8e46e81b9589d28918bf40e44270f19d6
kr95656 <katmsg8239@gmail.com>
2020-05-22 12:06:30 +0900

    item 削除、詳細アクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,15 @@ class ItemsController < ApplicationController
     Item.create(item_params)
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
+  def show
+    @item =  Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:title, :image, :text, tag_ids: []).merge(user_id: current_user.id)
