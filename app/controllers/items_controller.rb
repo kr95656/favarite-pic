5c1d3de0c994c98c3d76548fe46833e372cd5f3f
kr95656 <katmsg8239@gmail.com>
2020-05-22 17:50:18 +0900

    third commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,9 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
     @item = Item.find(params[:id])
+    @comments = @item.comments.includes(:user)
   end
 
   def edit
