5190b8f9b26507aa53d3773274d99a202dacd4b4
kr95656 <katmsg8239@gmail.com>
2020-05-25 20:04:58 +0900

    commit

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,6 +7,8 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
     @tags = Tag.all
+    items = Item.all
+    @tags_pagenate = @tag.items.order(created_at: :desc).page(params[:page]).per(10)
   end
 
 end
