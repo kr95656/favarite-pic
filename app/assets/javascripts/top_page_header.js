1f8648ad297462919f711c964594417db4639177
kr95656 <katmsg8239@gmail.com>
2020-06-08 02:16:24 +0900

    Add slick header

---
+++ app/assets/javascripts/top_page_header.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('.slider').slick({
+    // prevArrow:'<i class="fa fa-angle-left arrow arrow-left"></i>',
+    // nextArrow:'<i class="fa fa-angle-right arrow arrow-right"></i>',
+    fade: true,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 800,
+    infinite: true,
+  });
+
+});
