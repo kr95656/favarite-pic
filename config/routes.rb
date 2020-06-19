2c56d2f161caf279a05a71bb820c9a71903063b7
kr95656 <katmsg8239@gmail.com>
2020-06-19 14:45:25 +0900

    Add description controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,28 @@
 Rails.application.routes.draw do
+  
   devise_for :users, :controller => {
-    
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
   } 
+  
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+
+
   root "items#index"
+
   resources :tags,  only: [:index, :new, :create, :show] 
+
   resources :items, only: [:index, :new, :create, :destroy, :show, :edit, :update] do
     resources :comments, only: [:create]
     collection do
       get 'search'
     end
   end
+
   resources :users, only: [:edit, :update, :show]
+
 end
