4f6d7fc0e4c2f81db9108d346c2cacdc49220e9b
kr95656 <katmsg8239@gmail.com>
2020-06-30 22:31:24 +0900

    create before_action show_tag method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,31 +1,26 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: %i[index show search]
-  # before_action :authenticate_user! , only: [:new]
+  before_action :show_tag, only: %i[index new create show]
 
   def index
     @items = Item.all.includes(:user).order(created_at: :desc).page(params[:page]).per(21)
-    @tags = Tag.all
+    # @tags = Tag.all
   end
 
   def new
     @item = Item.new
-    @tags = Tag.all
+    # @tags = Tag.all
 
-    2.times { @item.tags.build }
+    # 2.times { @item.tags.build }
   end
 
   def create
-    # Item.create(item_params)
-
-    # Item.create(item_params)
-    @tags = Tag.all
+    # @tags = Tag.all
     @item = Item.new(item_params)
 
     if @item.save
       flash[:notice] = '投稿が完了しました。'
       redirect_to action: "index"
-      #  render :index
-      # render :index
     else # 作れてなかったら
       flash.now[:alert] = '画像URL,タイトル,タグを入力してください。'
       render :new
@@ -33,8 +28,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    # item = Item.find(params[:id])
-    # item.destroy
     item = Item.find(params[:id])
 
     if item.destroy
@@ -49,7 +42,7 @@ class ItemsController < ApplicationController
   def show
     @comment = Comment.new
     @item = Item.find(params[:id])
-    @tags = Tag.all
+    # @tags = Tag.all
     @comments = @item.comments.includes(:user)
   end
 
@@ -82,16 +75,8 @@ class ItemsController < ApplicationController
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
-  # def item_params
-  #   params.require(:item).permit(:title, :image, :text, tag_ids: []).merge(user_id: current_user.id)
-  # end
 
-  # # 追加
-  # def tag_params
-  #   params.require(:tag).permit(:id)
-  # end
-
-  # def item_params
-  #   params.require(:item).permit(:title, :image, :text, tag_ids: []).merge(user_id: current_user.id)
-  # end
+  def show_tag
+    @tags = Tag.all
+  end
 end
