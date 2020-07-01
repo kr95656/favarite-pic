aab4e37dff570d0b0cf5def3100c3ad9bba43dc9
kr95656 <katmsg8239@gmail.com>
2020-07-01 18:32:38 +0900

    Update tags_controller.rb

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,17 +1,19 @@
 class TagsController < ApplicationController
+  before_action :tag_show, only: %i[index show]
+
   def index
-    @tags = Tag.all
+    # @tags = Tag.all
   end
 
   def show
-    # @tag = Tag.find(params[:id])
-    # @tags = Tag.all
-    # items = Item.all
-    # @tags_pagenate = @tag.items.order(created_at: :desc).page(params[:page]).per(10)
-
     @tag = Tag.find(params[:id])
-    @tags = Tag.all
-    # items = Item.all
+    # @tags = Tag.all
     @tags_pagenate = @tag.items.order(created_at: :desc).page(params[:page]).per(10)
+  end
+
+  private
+
+  def tag_show
+    @tags = Tag.all
   end
 end
